381914a8d7befd4a82dc111b29d159d75c0beef5
Emma Tong <eat2004@gmail.com>
2020-01-12 10:19:02 -0500

    Added algorithim to move to target color
    If robot's color sensor detects a color that is 2 colors away from the target color, will stop motor so that control panel's sensor aligns with the target color.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.ColorSensor;
@@ -23,17 +24,20 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-  private ColorSensor m_colorSensor;
+  public static ColorSensor m_colorSensor;
+  public static Color colorTarget=Robot.m_colorSensor.kBlueTarget; //target to be changed
 
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
+
   @Override
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    m_colorSensor=new ColorSensor();
   }
 
   /**
